0f38cb2e8f77d39eb3a15bc3d57e1e24097c80fe
tim-oe <tecronin@gmail.com>
2024-12-13 12:38:57 -0600

    Squashed commit of the following:
    commit f5ebb0396cf30e34813daa3e0065eb68f4e75f3b
    Merge: cbee494 7172941
    Author: tim-oe <tecronin@gmail.com>
    Date:   Fri Dec 13 10:16:04 2024 -0600
    
        Merge branch 'main' of github.com:tim-oe/WeatherWatch into wu
    
    commit cbee494ce127dd1db939beef6829620d755901e6
    Author: tim-oe <tecronin@gmail.com>
    Date:   Thu Dec 12 21:50:46 2024 -0600
    
        added doc
    
    commit 8e2f313ffed0621be3c0674472a9100b5e58327e
    Author: tim-oe <tecronin@gmail.com>
    Date:   Thu Dec 12 21:48:48 2024 -0600
    
        added retry logic

--- weatherwatch/wu/WUClient.py
+++ weatherwatch/wu/WUClient.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from urllib.parse import ParseResult, urlparse
 
+import backoff
 import requests
 from conf.AppConfig import AppConfig
 from conf.WUConfig import WUConfig
 from py_singleton import singleton
+from requests import RequestException
 from util.Converter import Converter
 from util.Logger import logger
 from wu.WUData import WUData
@@ -19,6 +21,7 @@ class WUClient:
     post data to weather underground
     https://support.weather.com/s/article/PWS-Upload-Protocol?language=en_US
     https://docs.google.com/document/d/1eKCnKXI9xnoMGRRzOL1xPCBihNV2rOet08qpE_gArAY/edit?pli=1&tab=t.0
+    https://github.com/litl/backoff
     """
 
     END_POINT_URL: ParseResult = urlparse("https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php")
@@ -26,8 +29,8 @@ class WUClient:
     def __init__(self):
         self._wuConfig: WUConfig = AppConfig().wu
 
+    @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_tries=AppConfig().wu.retries, jitter=None)
     def post(self, readTime: datetime, data: WUData, endPoint: ParseResult = END_POINT_URL):
-
         data.stationId(self._wuConfig.stationId)
         data.stationKey(self._wuConfig.stationKey)
         data.readTime(Converter.to_utc(readTime))
@@ -36,4 +39,4 @@ class WUClient:
         self.logger.debug("request %s", resp.url)
 
         if resp.status_code != 200:
-            raise requests.RequestException(f"failed to post to weather underground {resp.status_code} {resp.reason}")
+            raise RequestException(f"failed to post to weather underground {resp.status_code} {resp.reason}")
