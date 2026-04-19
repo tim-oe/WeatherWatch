217a5fafa22aea8bd32efe10e739943b6d506aa9
tim-oe <tecronin@gmail.com>
2026-04-19 17:38:16 -0500

    migrate pi metrics to datetime

--- weatherwatch/entity/PIMetrics.py
+++ weatherwatch/entity/PIMetrics.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from entity.BaseEntity import BaseEntity
-from sqlalchemy import DateTime, Integer, Numeric
+from sqlalchemy import Integer, Numeric
+from sqlalchemy.dialects.mysql import DATETIME
 from sqlalchemy.orm import Mapped, mapped_column
 
 __all__ = ["PIMetrics"]
@@ -17,7 +18,7 @@ class PIMetrics(BaseEntity):
     __tablename__ = "pi_metrics"
     __table_args__ = {"extend_existing": True}
 
-    read_time: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=None)
+    read_time: Mapped[datetime] = mapped_column(DATETIME(fsp=6), nullable=False, default=None)
     cpu_temp_c: Mapped[float] = mapped_column(Numeric, nullable=False, default=None)
 
     mem_available: Mapped[int] = mapped_column(Integer, nullable=False, default=None)
